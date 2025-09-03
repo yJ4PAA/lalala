6de0744e14f57b280c5953fe0cade71a95e9d1c1
Ins0lin <48128616+Ins0lin@users.noreply.github.com>
2025-09-03 13:04:41 -0300

    Correção de espaçamentos

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
 let gem = document.querySelector('.gem-cost') //definiu a gema
 let parsedGem = parseFloat(gem.innerHTML) 
-
-
 let clickerCost = document.querySelector('.clicker-cost') //definiu o custo do clicker
 let parsedClickerCost = parseFloat(clickerCost.innerHTML)
 let clickerLevel = document.querySelector('.clicker-level') //definiu o nivel do clicker
 let clickerIncrease = document.querySelector('.clicker-increase') //definiu o aumento do clicker
 let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML)
-
 let pickaxeCost = document.querySelector('.pickaxe-cost') //definiu o custo da pickaxe
 let parsedPickaxeCost = parseFloat(pickaxeCost.innerHTML)
 let pickaxeLevel = document.querySelector('.pickaxe-level') //definiu o nivel da pickaxe
 let pickaxeIncrease = document.querySelector('.pickaxe-increase') //definiu o aumento da pickaxe
 let parsedPickaxeIncrease = parseFloat(pickaxeIncrease.innerHTML)
-
 let minerCost = document.querySelector('.miner-cost') //definiu o custo do miner
 let parsedMinerCost = parseFloat(minerCost.innerHTML)
 let minerLevel = document.querySelector('.miner-level') //definiu o nivel do miner
 let minerIncrease = document.querySelector('.miner-increase') //definiu o aumento do miner
 let parsedMinerIncrease = parseFloat(minerIncrease.innerHTML)
-
 let gpcText = document.getElementById('gpc-text') //definiu o texto do gps e gpc
 let gpsText = document.getElementById('gps-text') 
 let gpc = 1;
 let gps = 0;
-
 function incrementGem() {  //função de adição ao clicar na gema
     gem.innerHTML = Math.round(parsedGem += gpc)
 }
-
 function buyClicker() { //comprar o upgrade clicker
     if (parsedGem >= parsedClickerCost) {
         gem.innerHTML = Math.round(parsedGem -= parsedClickerCost);
@@ -43,7 +36,6 @@ function buyClicker() { //comprar o upgrade clicker
         clickerCost.innerHTML = Math.round(parsedClickerCost)
     }
 }
-
 function buyPickaxe() { //comprar o upgrade Pickaxe
     if (parsedGem >= parsedPickaxeCost) {
         gem.innerHTML = Math.round(parsedGem -= parsedPickaxeCost);
@@ -58,7 +50,6 @@ function buyPickaxe() { //comprar o upgrade Pickaxe
         pickaxeCost.innerHTML = Math.round(parsedClickerCost)
     }
 }
-
 function buyMiner() { //comprar o upgrade Miner
     if (parsedGem >= parsedMinerCost) {
         gem.innerHTML = Math.round(parsedGem -= parsedMinerCost);
@@ -73,7 +64,6 @@ function buyMiner() { //comprar o upgrade Miner
         minerCost.innerHTML = Math.round(parsedClickerCost)
     }
 }
-
 setInterval(() => {
     parsedGem += gps / 10
     gem.innerHTML = Math.round(parsedGem)
