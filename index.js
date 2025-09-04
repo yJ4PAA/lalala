42b2e1c5f09c577409616c4e1be8fce8ccdeb343
yJ4PAA <lil.japa999@gmail.com>
2025-09-04 12:26:55 -0300

    i si amigo!

--- index.js
+++ index.js
@@ -12,50 +12,7 @@ let gemImgContainer = document.querySelector('.gem-img-container')
 let gpc = 1;
 let gps = 0;
 
-/*
-const upgrades = [
-    {
-        name: 'clicker',
-        cost: document.querySelector('.clicker-cost'),
-        parsedCost: parseFloat(document.querySelector('.clicker-cost').innerHTML),
-        increase: document.querySelector('.clicker-increase'),
-        parsedIncrease: parseFloat(document.querySelector('.clicker-increase').innerHTML),
-        level: document.querySelector('.clicker-level'),
-        gemMultiplier: 1.025,
-        costMultiplier: 1.12,
-    },
-    {
-        name: 'pickaxe',
-        cost: document.querySelector('.pickaxe-cost'),
-        parsedCost: parseFloat(document.querySelector('.pickaxe-cost').innerHTML),
-        increase: document.querySelector('.pickaxe-increase'),
-        parsedIncrease: parseFloat(document.querySelector('.pickaxe-increase').innerHTML),
-        level: document.querySelector('.pickaxe-level'),
-        gemMultiplier: 1.03,
-        costMultiplier: 1.112,
-    },
-    {
-        name: 'miner',
-        cost: document.querySelector('.miner-cost'),
-        parsedCost: parseFloat(document.querySelector('.miner-cost').innerHTML),
-        increase: document.querySelector('.miner-increase'),
-        parsedIncrease: parseFloat(document.querySelector('.miner-increase').innerHTML),
-        level: document.querySelector('.miner-level'),
-        gemMultiplier: 1.035,
-        costMultiplier: 1.11,
-    },
-    {
-        name: 'factory',
-        cost: document.querySelector('.factory-cost'),
-        parsedCost: parseFloat(document.querySelector('.factory-cost').innerHTML),
-        increase: document.querySelector('.factory-increase'),
-        parsedIncrease: parseFloat(document.querySelector('.factory-increase').innerHTML),
-        level: document.querySelector('.factory-level'),
-        gemMultiplier: 1.04,
-        costMultiplier: 1.10,
-    }
-]
-*/
+
 function createUpgrades() {
     const upgradesContainer = document.getElementById('upgrades-container')
     const template = document.getElementById('upgrade-template').textContent
